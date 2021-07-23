b19c2f90b7bd03bb066881dd7a487323febba6e3
thogra <thgr02@gmail.com>
2021-07-23 14:25:51 +0200

    Scrape article pages (for text)

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,5 +1,4 @@
 const cheerio = require('cheerio');
-const sizeOf = require('image-size');
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const { cleanHref } = require('./rodong-utils');
@@ -12,14 +11,46 @@ const fetchImagePage = async (base, imagePath) => {
   const $ = cheerio.load(body);
   const imageElements = $('#slides .slide img').toArray();
 
-  return _.map(imageElements, image => {
-    return { 
+  return _.map(imageElements, (image) => {
+    return {
       url: baseUrl + $(image).attr('src'),
       height: $(image).attr('height'),
       height: $(image).attr('width'),
-  }
+    };
   });
-}
+};
+
+const fetchArticlePage = async (articleUrl) => {
+  const response = await fetch(`${articleUrl}`);
+  if (response.status !== 200) {
+    console.error(
+      `Got response status ${response.status} for url: `,
+      articleUrl,
+    );
+    return undefined;
+  }
+
+  const body = await response.text();
+  const $ = cheerio.load(body);
+  const articleContainer = $('body table').first();
+  const title = $(articleContainer)
+    .find('.ArticleContent')
+    .first()
+    .find('font')
+    .text();
+  const allContent = $(articleContainer).find('.ArticleContent').toArray();
+  const paragraphs = _.slice(allContent, 1, allContent.length - 1);
+  const paragraphText = _.compact(
+    _.map(paragraphs, (paragraph) => _.trim($(paragraph).text())),
+  );
+  const byline = $(_.last(allContent)).text();
+
+  return {
+    title,
+    content: paragraphText,
+    byline,
+  };
+};
 
 const fetchBasePage = async () => {
   const response = await fetch(baseUrl + '/en/');
@@ -35,29 +66,42 @@ const fetchBasePage = async () => {
     .find('td > a')
     .toArray();
 
-  const pureLinks = _.compact(_.map(revolutionaryLinks, (linkElement) => {
-    const href = $(linkElement).attr('href');
-    const imgLink = $(linkElement).siblings('nobr').find('a').attr('href');
+  const pureLinks = _.compact(
+    _.map(revolutionaryLinks, (linkElement) => {
+      const href = $(linkElement).attr('href');
+      const imgLink = $(linkElement).siblings('nobr').find('a').attr('href');
 
-    if (_.startsWith(href, 'javascript:article_open(')) {
-      return {
-        url: `${baseUrl}/en/${cleanHref(href)}`,
-        image: cleanHref(imgLink),
-      };
-    }
-    return undefined;
-  }));
-  
-  const withImages = await Promise.all(_.map(pureLinks ,async (item) => {
-    if (item.image) {
-      const images = await fetchImagePage(baseUrl, item.image);
-      return {...item, images };
-    }
-    return item;
-  }));
+      if (_.startsWith(href, 'javascript:article_open(')) {
+        return {
+          url: `${baseUrl}/en/${cleanHref(href)}`,
+          imagePage: cleanHref(imgLink),
+        };
+      }
+      return undefined;
+    }),
+  );
 
-  console.log('scraped::', withImages);
+  const withImages = await Promise.all(
+    _.map(pureLinks, async (item) => {
+      if (item.imagePage) {
+        const images = await fetchImagePage(baseUrl, item.imagePage);
+        return { ...item, images };
+      }
+      return item;
+    }),
+  );
 
+  const withContents = await Promise.all(
+    _.map(withImages, async (item) => {
+      if (item.url) {
+        const articleData = await fetchArticlePage(item.url);
+        return { ...item, ...articleData };
+      }
+      return item;
+    }),
+  );
+
+  console.log('scraped::', withContents);
   return body;
 };
 
